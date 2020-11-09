20834323fd18b3c1ed6792565519fd2bcd505072
manjunath387 <73433702+manjunath387@users.noreply.github.com>
2020-11-09 23:39:09 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,7 @@
+const toogleButton=document.getElementsByClassName('toggle-btn')[0]
+const navLinks=document.getElementsByClassName('nav-links')[0]
+
+
+toogleButton.addEventListener('click',()=>{
+    navLinks.classList.toggle('active')
+})
